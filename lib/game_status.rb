d23026283c0dd76155ff79be674c0bc3b935efa3
jose tavarez <newyorkcityjose@gmail.com>
2019-11-15 20:22:25 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,19 +7,19 @@ end
 WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,4,8], [2,4,6], [0,3,6], [2,5,8], [1,4,7]]
 
 def won?(board)
-  WIN_COMBINATIONS.each {|win_combo|
-    index_0 = win_combo[0]
-    index_1 = win_combo[1]
-    index_2 = win_combo[2]
+  WIN_COMBINATIONS.each {|win_combination|
+    index_0 = win_combination[0]
+    index_1 = win_combination[1]
+    index_2 = win_combination[2]
 
     position_1 = board[index_0]
     position_2 = board[index_1]
     position_3 = board[index_2]
 
     if position_1 == "X" && position_2 == "X" && position_3 "X"
-      return win_combo
+      return win_combination
     elsif position_1 == "O" && position_2 "O" && position_3 "O"
-      return win_combo
+      return win_combination
     end
 
     }
