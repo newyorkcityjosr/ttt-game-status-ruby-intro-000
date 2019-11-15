1e5969dfc064a542448d15e42a0e80fca8ed2957
jose tavarez <newyorkcityjose@gmail.com>
2019-11-15 21:30:47 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,7 +7,7 @@ end
 WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2]]
 
 def won?(board)
-  WIN_COMBINATIONS.each do |win_combination|
+  WIN_COMBINATIONS.each{ |win_combination|
     index_0 = win_combination[0]
     index_1 = win_combination[1]
     index_2 = win_combination[2]
@@ -21,10 +21,10 @@ def won?(board)
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combination
     end
-false
+}
+return false
+
 end
-
-
 
 def full?(board)
   board.all? {|index| index == "X" || index == "O"}
