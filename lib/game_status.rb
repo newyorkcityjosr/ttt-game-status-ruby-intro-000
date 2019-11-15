748b5b4e588a6e1a7f9924e43f3c8f59961bddd7
jose tavarez <newyorkcityjose@gmail.com>
2019-11-15 20:25:46 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,9 +16,9 @@ def won?(board)
     position_2 = board[index_1]
     position_3 = board[index_2]
 
-    if position_1 == "X" && position_2 == "X" && position_3 "X"
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
       return win_combination
-    elsif position_1 == "O" && position_2 "O" && position_3 "O"
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combination
     end
 
