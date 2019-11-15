f1d6e53623d0caff0c8282e6d7c4aaa0457a3fb2
jose tavarez <newyorkcityjose@gmail.com>
2019-11-15 20:48:00 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -7,7 +7,7 @@ end
 WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [6,4,2]]
 
 def won?(board)
-  WIN_COMBINATIONS.each{ |win_combination|
+  WIN_COMBINATIONS.each{ |WIN_COMBINATIONS|
     index_0 = win_combination[0]
     index_1 = win_combination[1]
     index_2 = win_combination[2]
